57c01b7b2d80829363afb2d21dcdcaac2addf650
Łukasz Pęgiel <me@abapblog.com>
2017-06-29 19:04:34 +0200

    Initial commit

---
+++ ABAP Favorites/src/com/abapblog/favorites/views/ILinkedWithEditorView.java
@@ -0,0 +1,19 @@
+package com.abapblog.favorites.views;
+
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IViewSite;
+
+public interface ILinkedWithEditorView {
+	  /**
+	   * Called when an editor is activated
+	   * e.g. by a click from the user.
+	   * @param The activated editor part.
+	   */
+
+	  void editorActivated(IEditorPart activeEditor);
+
+	  /**
+	   * @return The site for this view.
+	   */
+	  IViewSite getViewSite();
+}
