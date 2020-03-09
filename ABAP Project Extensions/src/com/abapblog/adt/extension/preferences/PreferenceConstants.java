286c07f9b16fd857b777e9a1698f9c60096ef322
Łukasz Pęgiel <me@abapblog.com>
2020-03-09 22:29:58 +0100

    split of plugins

--- ABAP Project Extensions/src/com/abapblog/adt/extension/preferences/PreferenceConstants.java
+++
@@ -1,8 +0,0 @@
-package com.abapblog.adt.extension.preferences;
-
-public class PreferenceConstants {
-
-	public static final String doAutomaticLogonAtStart = "doAutomaticLogonAtStart";
-	public static final String AutomaticLogonOnlyForStoredPasswords = "AutomaticLogonOnlyForStoredPasswords";
-
-}
